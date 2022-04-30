027f68f6d9c8d5c3f42da6cca0202e5006685c6f
Kadika <dryan10101@gmail.com>
2022-04-30 16:23:47 -0700

    Fixed several issues, added small visual changes, and mopped up some ugly test code

--- script.js
+++ script.js
@@ -40,6 +40,28 @@ var tempSix = document.querySelector("#tempSix");
 var windSix = document.querySelector("#windSix");
 var humiditySix = document.querySelector("#humiditySix");
 
+if (localStorage.getItem("listySto")) {
+    var listySto = JSON.parse(localStorage.getItem("listySto"));
+    for (i = 0; i < listySto.length; i++) {
+        var listy = document.querySelector("#listy");
+        var liAdd = document.createElement("li");    
+        listy.appendChild(liAdd);
+        var lastAddedLi = listy.lastElementChild;
+        var btnAdd = document.createElement("button");
+        btnAdd.textContent = listySto[i];
+        btnAdd.setAttribute("class", "useFull");
+        btnAdd.addEventListener("click", function() {
+            otherBtnHandler(this.textContent);
+        })
+        lastAddedLi.appendChild(btnAdd);
+        var brAdd = document.createElement("br");
+        listy.appendChild(brAdd);
+    }
+}
+else {
+    var listySto = [];
+}
+
 btn.addEventListener("click", function() {
     btnClickHandler();
 });
@@ -57,7 +79,6 @@ function btnClickHandler() {
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
         //build current weather API call
         lat = data[0].lat;
         lon = data[0].lon;
@@ -67,12 +88,20 @@ function btnClickHandler() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
             //update current weather html
             tempT.textContent = data.current.temp;
             windT.textContent = data.current.wind_speed;
             humidityT.textContent = data.current.humidity;
             uvT.textContent = data.current.uvi;
+            if (uvT.textContent < 2) {
+                uvT.setAttribute("style", "background-color: greenyellow"); 
+            }
+            if (uvT.textContent > 2) {
+                uvT.setAttribute("style", "background-color: orange");
+            }
+            if (uvT.textContent > 6) {
+                uvT.setAttribute("style", "background-color: red");
+            }
             weatherIconT.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
             //update todays date
             dateT.textContent = moment().format("M/D/YYYY");
@@ -83,7 +112,131 @@ function btnClickHandler() {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
+                //5 13 21 29 37
+                //getting, formatting, and displaying dates
+                dateTwoOrig = data.list[5].dt_txt;
+                dateTwoOrig = dateTwoOrig.split(' ')[0];
+                dateTwoOrig = moment(dateTwoOrig).format("M/D/YYYY");
+                dateThreeOrig = data.list[13].dt_txt;
+                dateThreeOrig = dateThreeOrig.split(' ')[0];
+                dateThreeOrig = moment(dateThreeOrig).format("M/D/YYYY");
+                dateFourOrig = data.list[21].dt_txt;
+                dateFourOrig = dateFourOrig.split(' ')[0];
+                dateFourOrig = moment(dateFourOrig).format("M/D/YYYY");
+                dateFiveOrig = data.list[29].dt_txt;
+                dateFiveOrig = dateFiveOrig.split(' ')[0];
+                dateFiveOrig = moment(dateFiveOrig).format("M/D/YYYY");
+                dateSixOrig = data.list[37].dt_txt;
+                dateSixOrig = dateSixOrig.split(' ')[0];
+                dateSixOrig = moment(dateSixOrig).format("M/D/YYYY");
+                dateTwo.textContent = dateTwoOrig;
+                dateThree.textContent = dateThreeOrig;
+                dateFour.textContent = dateFourOrig;
+                dateFive.textContent = dateFiveOrig;
+                dateSix.textContent = dateSixOrig;
+                //getting and displaying weather icons
+                weatherIconTwo.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[5].weather[0].icon + "@2x.png");
+                weatherIconThree.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[13].weather[0].icon + "@2x.png");
+                weatherIconFour.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[21].weather[0].icon + "@2x.png");
+                weatherIconFive.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[29].weather[0].icon + "@2x.png");
+                weatherIconSix.setAttribute("src", "http://openweathermap.org/img/wn/" + data.list[37].weather[0].icon + "@2x.png");
+                //getting and displaying temp, wind, and humidity
+                tempTwo.textContent = data.list[5].main.temp;
+                windTwo.textContent = data.list[5].wind.speed;
+                humidityTwo.textContent = data.list[5].main.humidity;
+                tempThree.textContent = data.list[13].main.temp;
+                windThree.textContent = data.list[13].wind.speed;
+                humidityThree.textContent = data.list[13].main.humidity;
+                tempFour.textContent = data.list[21].main.temp;
+                windFour.textContent = data.list[21].wind.speed;
+                humidityFour.textContent = data.list[21].main.humidity;
+                tempFive.textContent = data.list[29].main.temp;
+                windFive.textContent = data.list[29].wind.speed;
+                humidityFive.textContent = data.list[29].main.humidity;
+                tempSix.textContent = data.list[37].main.temp;
+                windSix.textContent = data.list[37].wind.speed;
+                humiditySix.textContent = data.list[37].main.humidity;
+
+                //make rightSec appear
+                rightSec.style.display = "block";
+            })
+        })
+    })
+    //update list on left side
+    
+
+    //update list on left side and local storage list
+    var hasCity = false;
+    for (i = 0; i < listySto.length; i++) {
+        if (listySto[i] == city) {
+            hasCity = true;
+        }
+    }
+    if (!(hasCity)) {
+        var listy = document.querySelector("#listy");
+        var liAdd = document.createElement("li");    
+        listy.appendChild(liAdd);
+        var lastAddedLi = listy.lastElementChild;
+        var btnAdd = document.createElement("button");
+        btnAdd.textContent = city;
+        btnAdd.setAttribute("class", "useFull");
+        btnAdd.addEventListener("click", function() {
+            otherBtnHandler(btnAdd.textContent);
+        })
+        lastAddedLi.appendChild(btnAdd);
+        var brAdd = document.createElement("br");
+        listy.appendChild(brAdd);
+
+        listySto.push(city);
+    }
+    localStorage.setItem("listySto", JSON.stringify(listySto));
+};
+
+function otherBtnHandler(stringy) {
+    //build geocoding API call
+    var city = stringy;
+    place.textContent = city;
+    var geo = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apikey;
+    var lat;
+    var lon;
+    fetch(geo)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        //build current weather API call
+        lat = data[0].lat;
+        lon = data[0].lon;
+        var openweather = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apikey;
+        fetch(openweather)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //update current weather html
+            tempT.textContent = data.current.temp;
+            windT.textContent = data.current.wind_speed;
+            humidityT.textContent = data.current.humidity;
+            uvT.textContent = data.current.uvi;
+            if (uvT.textContent < 2) {
+                uvT.setAttribute("style", "background-color: greenyellow"); 
+            }
+            if (uvT.textContent > 2) {
+                uvT.setAttribute("style", "background-color: orange");
+            }
+            if (uvT.textContent > 6) {
+                uvT.setAttribute("style", "background-color: red");
+            }
+            weatherIconT.setAttribute("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png");
+            //update todays date
+            dateT.textContent = moment().format("M/D/YYYY");
+            //get 5 day forecast
+            var fiveDayWeather = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + apikey;
+            fetch(fiveDayWeather)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
                 //5 13 21 29 37
                 //getting, formatting, and displaying dates
                 dateTwoOrig = data.list[5].dt_txt;
@@ -134,4 +287,4 @@ function btnClickHandler() {
 
     //make rightSec appear
     rightSec.style.display = "block";
-};
+}
